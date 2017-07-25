8ef8fcfa5e46b4dadef9fac007a4ad60d9d27cbf
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2017-07-25 18:02:31 +0200

    Unterscheidung der Host(hard)states in den Markerfarben

--- public/js/module.js
+++ public/js/module.js
@@ -8,8 +8,8 @@
     };
 
     var map;
-    var layers;
     var markers;
+    var hostMarkers;
 
     Map.prototype = {
         initialize: function()
@@ -19,45 +19,87 @@
         },
 
         registerTimer: function () {
+            if(this.timer) {
+                this.module.icinga.timer.unregister(this.timer); 
+            }
             this.timer = this.module.icinga.timer.register(
                 this.updateMapData,
                 this,
-                300000
+                10000
             );
             return this;
         },
 
         updateMapData: function () {
             xhr = new XMLHttpRequest();
-            xhr.open('GET', 'map/mapdata', true);
+            xhr.open('GET', 'map/data/points', true);
             xhr.onreadystatechange = function(e) {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
                         var result = JSON.parse(xhr.responseText);
 
-                        markers.clearLayers();
-                        map.removeLayer(markers);
+                        // remove old markers
+                        $.each( hostMarkers, function( hostname, d ) {
+                            if(!result[hostname]) {
+                                markers.removeLayer(d);
+                                delete hostMarkers[hostname];
+                            }
+                        });
 
-                        $.each( result, function( id, data ) {
-                            var info = '<table>';
-                            $.each( data['nodes'], function( hostname, node ) {
-                                var services = node['services'].length
-                                info += '<tr><th colspan="2">'+hostname+'</th></tr>';
-                                info += '<tr><td>Services:</td><td>'+services+'</td>';
+                        $.each( result, function( hostname, data ) {
+                            var hard_state = data['host_hard_state'];
+                            var icon;
+
+                            switch(hard_state) {
+                                case 0:
+                                    icon = greenMarker;
+                                    break;
+                                case 1:
+                                    icon = redMarker;
+                                    break;
+                                default:
+                                    icon = blueMarker;
+                            }
+
+                            var service_status = {};
+                            service_status[0] = "OK";
+                            service_status[1] = "WARN";
+                            service_status[2] = "CRIT";
+                            service_status[3] = "UNKOWN";
+                            service_status[99] = "PENDING";
+
+                            var services = '<table>';
+                            services += '<tr><th colspan="2">Service overview</th></tr>';
+                            $.each( data['services'], function( service_display_name, service ) {
+                                services += '<tr><td class="map_service_state map_service_state'
+                                    + service['service_hard_state']
+                                    + '">'
+                                    + service_status[service['service_hard_state']]
+                                    + '</td><td class="map_service_name">'
+                                    + service_display_name
+                                    + '</td></tr>'
                             });
+                            services += '</table>';
 
-                            info += '</table>';
+                            var info = '<div class="map_host_detail">';
+                            info += '<p><a href="monitoring/host/show?host='+hostname+'">'+hostname+'</a></p>'
+                            info += services;
+                            info += '</div>';
 
-                            var markerIcon = L.icon({
-                                iconUrl: 'img/map/marker-icon.png',
-                                shadowUrl: 'img/map/marker-shadow.png',}
-                            );
+                            var marker;
 
-                            var marker = L.marker(data['coordinates'], {icon: markerIcon}).addTo(markers);
+                            if(hostMarkers[hostname]) {
+                                marker = hostMarkers[hostname]; 
+                            } else {
+                                marker = L.marker(data['coordinates'], {icon: icon}).addTo(markers);
+                                hostMarkers[hostname] = marker
+                                markers.addTo(map);
+                            }
+
+                            marker.setIcon(icon);
                             marker.bindPopup(info);
                         });
 
-                        markers.addTo(map);
                     }
                 }
             };
@@ -65,11 +107,10 @@
         },
 
         onRenderedContainer: function(event) {
-            markers = new L.MarkerClusterGroup({
-               // iconCreateFunction: function(cluster) {
-               //     return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
-               // }
-            });
+            // TODO: initialize once and only update
+            markers = new L.MarkerClusterGroup();
+            hostMarkers = {};
+
             // TODO: move coordinates to configuration
             map = L.map('map').setView([52.50146, 13.37096], 6);
 
@@ -80,7 +121,7 @@
             var osm = L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
                 subdomains: ['a','b','c'],
-                maxZoom: 17,
+                maxZoom: 19,
                 minZoom: 5,
             });
 
