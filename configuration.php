2da2c723577e0a09119330bb6ccc9099b8dc365e
ENDA Administrator <admin@enda.eu>
2017-07-25 13:40:18 +0200

    Markericons repariert

--- configuration.php
+++ configuration.php
@@ -3,6 +3,8 @@ $this->menuSection('Hostmap')
 	->setUrl('map')
 	->setIcon('globe');
 
+//$this->provideCssFile('third-party/font-awesome/css/font-awesome.css');
+
 $this->provideCssFile('third-party/leaflet.css');
 $this->provideCssFile('third-party/MarkerCluster.css');
 $this->provideCssFile('third-party/MarkerCluster.Default.css');
