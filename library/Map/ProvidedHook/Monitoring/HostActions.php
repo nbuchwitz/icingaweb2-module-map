6ff0e0701db1280f7022b3e821ab8e3b1f1f1a58
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2018-01-22 15:02:17 +0100

    Added Hook for Cube & show-on-map link only if geolocation var exists

--- library/Map/ProvidedHook/Monitoring/HostActions.php
+++ library/Map/ProvidedHook/Monitoring/HostActions.php
@@ -2,6 +2,7 @@
 
 namespace Icinga\Module\Map\ProvidedHook\Monitoring;
 
+use Icinga\Module\Monitoring\DataView\Customvar;
 use Icinga\Web\Controller\ModuleActionController;
 use Icinga\Module\Monitoring\Hook\HostActionsHook;
 use Icinga\Module\Monitoring\Object\Host;
@@ -11,8 +12,13 @@ class HostActions extends HostActionsHook
 {
     public function getActionsForHost(Host $host)
     {
-        return array(
-            t("Show on map") => Url::fromPath('map/', array('showHost' => $host->host_name))
-        );
+        $actions = [];
+
+        $host->fetchCustomvars();
+        if (array_key_exists("geolocation", $host->customvars)) {
+            $actions[t("Show on map")] = Url::fromPath('map/', array('showHost' => $host->host_name));
+        }
+
+        return $actions;
     }
 }
