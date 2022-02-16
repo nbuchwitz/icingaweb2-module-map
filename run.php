cf149fa6ffc37f1c0e17ecf8b3ceea561978046a
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-02-16 11:32:59 +0100

    Add `IcingadbSupport` hook

--- run.php
+++ run.php
@@ -5,6 +5,7 @@ use OpenCage\Loader\CompatLoader;
 $this->provideHook('monitoring/HostActions');
 $this->provideHook('monitoring/ServiceActions');
 $this->provideHook('cube/Actions', 'CubeLinks');
+$this->provideHook('icingadb/IcingadbSupport');
 
 require_once __DIR__ . '/library/vendor/OpenCage/Loader/CompatLoader.php';
 CompatLoader::delegateLoadingToIcingaWeb($this->app);
