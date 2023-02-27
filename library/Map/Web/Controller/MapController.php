550c7e5fb177459ee59c9b45b4c15a3210d68496
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2023-02-27 13:44:42 +0100

    MapController: Remove superfluous code

--- library/Map/Web/Controller/MapController.php
+++ library/Map/Web/Controller/MapController.php
@@ -6,10 +6,6 @@ use Icinga\Application\Modules\Module;
 use Icinga\Module\Map\ProvidedHook\Icingadb\IcingadbSupport;
 use Icinga\Module\Map\Util\IcingadbUtils;
 use Icinga\Module\Monitoring\Controller;
-use ipl\Orm\Query;
-use ipl\Orm\UnionQuery;
-use ipl\Stdlib\Filter;
-use ipl\Web\Filter\QueryString;
 
 abstract class MapController extends Controller
 {
@@ -22,9 +18,6 @@ abstract class MapController extends Controller
     /** @var string Pattern to check for broken coordinates */
     protected $coordinatePattern = '/^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/';
 
-    /** @var Filter\Rule Filter from query string parameters */
-    private $filter;
-
     protected function moduleInit()
     {
         if (Module::exists('icingadb') && IcingadbSupport::useIcingaDbAsBackend()) {
@@ -35,36 +28,5 @@ abstract class MapController extends Controller
         }
 
         parent::moduleInit();
-    }
-
-    /**
-     * Get the filter created from query string parameters
-     *
-     * @return Filter\Rule
-     */
-    public function getFilter(): Filter\Rule
-    {
-        if ($this->filter === null) {
-            $this->filter = QueryString::parse((string) $this->params);
-        }
-
-        return $this->filter;
-    }
-
-    public function filter(Query $query, Filter\Rule $filter = null): self
-    {
-        if ($this->hasPermission('config/authentication/roles/show')) {
-            $this->icingadbUtils->applyRestrictions($query);
-        }
-
-        if ($query instanceof UnionQuery) {
-            foreach ($query->getUnions() as $query) {
-                $query->filter($filter ?: $this->getFilter());
-            }
-        } else {
-            $query->filter($filter ?: $this->getFilter());
-        }
-
-        return $this;
     }
 }
